45908268c5ff0990c4065ac8b5a94b8f25f13107
OszkárLeopold <oszkar.leopold@smartymind.hu>
2024-02-26 15:29:56 +0100

    BodyMassindex1 bekerült

---
+++ src/BodyMassindex1.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class BodyMassindex1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérlek add meg testsúlyodat kilogrammban: ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Kérlek add meg magasságodat méterben: ");
+        double height = scanner.nextDouble();
+
+        calculatorBMI1( weight , height);
+
+       // calculatorBMI2( weight , height);
+    }
+
+
+    public static void calculatorBMI1( double a, double b ) {
+        //Első operátorok
+        System.out.println( "Első fajta megoldás");
+        double valueBMI = a / ( b * b);
+        System.out.println("A számított BMI : " + valueBMI);
+
+        if (valueBMI < 18.5) {
+
+            System.out.println("Alultáplált");
+
+        }
+
+
+        if (valueBMI >= 18.5 && valueBMI <= 24.9) {
+
+            System.out.println("Normális");
+
+        }
+
+        if (valueBMI >= 25.0 && valueBMI <= 29.9) {
+
+            System.out.println("Túlsúlyos");
+
+        }
+
+
+        if (valueBMI >= 30.0) {
+
+            System.out.println("Elhízott");
+
+        }
+/*
+        public static void calculatorBMI2( double a, double b ) {
+            //Második operátorok
+            System.out.println( "Második fajta megoldás");
+            double c = 1 / b**;
+            double valueBMI = a * c;
+            System.out.println("A számított BMI : " + valueBMI);
+
+            if (valueBMI < 18.5) {
+
+                System.out.println("Alultáplált");
+
+            }
+
+
+            if (valueBMI >= 18.5 && valueBMI <= 24.9) {
+
+                System.out.println("Normális");
+
+            }
+
+            if (valueBMI >= 25.0 && valueBMI <= 29.9) {
+
+                System.out.println("Túlsúlyos");
+
+            }
+
+
+            if (valueBMI >= 30.0) {
+
+                System.out.println("Elhízott");
+
+            }
+
+        /* BMI Érték       Kategória
+        < 18.5        Alultáplált
+        18.5 - 24.9    Normális
+        25.0 - 29.9    Túlsúlyos
+        >= 30.0       Elhízott
+
+        BMI számítása: kg / (m * m)
+
+         */
+
+        }
+    }
+
